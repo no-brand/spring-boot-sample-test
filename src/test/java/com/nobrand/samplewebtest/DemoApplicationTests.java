6ed7b61383e18efb8d9617a3a8bb34c0b155de5c
no-brand <do.dream.david@gmail.com>
2021-07-25 23:21:54 +0900

    Apply Junit

--- src/test/java/com/nobrand/samplewebtest/DemoApplicationTests.java
+++ src/test/java/com/nobrand/samplewebtest/DemoApplicationTests.java
@@ -1,13 +1,47 @@
 package com.nobrand.samplewebtest;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+import java.nio.charset.StandardCharsets;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
-class DemoApplicationTests {
+@WebAppConfiguration
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class DemoApplicationTests {
+
+	private MockMvc mockMvc;
+
+	public DemoApplicationTests() { }
+
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
+	@Before
+	public void setup() throws Exception {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
 
 	@Test
-	void contextLoads() {
+	public void getAll() throws Exception {
+		this.mockMvc
+		 	.perform(MockMvcRequestBuilders.get("/movie/all"))
+		 	.andExpect(MockMvcResultMatchers.status().isOk());
 	}
 
 }
